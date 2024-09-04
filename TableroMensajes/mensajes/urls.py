b94d4fa2be9f547148c576925d0097890eb4fc6f
Gonzalo Gabriel Vega <ggvega@udc.edu.ar>
2024-09-04 17:32:05 -0300

    04-09-24 | 17:32:00

--- TableroMensajes/mensajes/urls.py
+++ TableroMensajes/mensajes/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.user_login, name='login'),
     path('crear/', views.CrearMensajes, name='crear'),
-    path('mensajes/', views.VerMensajes, name='mensajes')
+    path('mensajes/', views.VerMensajes, name='mensajes'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.logout, name='logout'),
 ]
--- TableroMensajes/mensajes/views.py
+++ TableroMensajes/mensajes/views.py
@@ -1,7 +1,41 @@
 from django.shortcuts import render, redirect
 from .models import Mensaje
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user= form.save()
+            login(request,user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'auth.html', {'form':form, 'is_login':False})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'auth.html', {'form': form, 'is_login': True})
+
+def user_logout(request):
+    logout(request)  # Cierra la sesión del usuario actual
+    return redirect('login')  # Redirige a la página principal después del cierre de sesión
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
+@login_required
 def CrearMensajes(request):
     if request.method == 'POST':
         remitente = request.user.username
@@ -13,5 +47,6 @@ def CrearMensajes(request):
             return redirect('home')
     return render(request, 'crear.html')        
 
+@login_required
 def VerMensajes(request):
     return render(request, 'mensajes.html')
