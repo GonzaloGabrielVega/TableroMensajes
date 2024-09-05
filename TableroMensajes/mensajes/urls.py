0b254f866c55ca1ef95047ecc7f19d4634942b05
Gonzalo Gabriel Vega <ggvega@udc.edu.ar>
2024-09-04 23:20:37 -0300

    04-09-24 | 23:20:25

--- TableroMensajes/mensajes/urls.py
+++ TableroMensajes/mensajes/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('home/', views.home, name='home'),
     path('', views.user_login, name='login'),
     path('crear/', views.CrearMensajes, name='crear'),
     path('mensajes/', views.VerMensajes, name='mensajes'),
     path('register/', views.register, name='register'),
-    path('logout/', views.logout, name='logout'),
+    path('logout/', views.user_logout, name='logout'),
 ]
